bcf328f62288b804ef9e838459d8213d9ec59a45
sumant <sumant.tedroox@gmail.com>
2021-01-02 12:32:53 +0530

    Changes Agents page and Banks Page and CoOperative Page  As per Figma Design

--- src/pages/AgentS/AgentS.tsx
+++ src/pages/AgentS/AgentS.tsx
@@ -1,76 +1,80 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { IonPage, IonContent, IonText, IonApp } from '@ionic/react';
-import { Translate } from '../../i18n/formatMessages';
-import { ButtonConmponent, InputText, HeaderComponent } from '../../components';
+import   { Translate   } from '../../i18n/formatMessages';
+import { useHistory } from 'react-router-dom';
+import { ButtonConmponent, HeaderComponent} from '../../components';
 import './AgentS.scss';
-
 const AgentS: React.FC = () => {
+  const history = useHistory();
+  const countrySelected=useState('XYZ');
+  const agentCode=useState('XYZ');
+  const accountHolderName=useState('Name of the account');
+  const accountNumber=useState('XYZ');
+  const mobileNumber=useState('XYZ');
+  const amount=useState('XYZ');
+  const remarks=useState('XYZ');
+
+   
+  function handleSucess() {
+    console.log('Handling registration');
+    history.push('/sucessPage');
+  }
   return (
     <>
       <IonApp>
         <IonPage>
           <HeaderComponent headerLable="common.header" />
           <IonContent>
-            <div className="container">
-              <IonText className="header-text-area">
-                <Translate message="agent.text" />
+            <div className="agentDetails-container">
+              <IonText className="agentDetails-text-area">
+                <Translate  message="agent.text"/>
               </IonText>
-              <div className="page-wrapper">
-                <InputText
-                  inputType="text"
-                  labelText="agent.country"
-                  labelType="floating"
-                  color="light"
-                  labelColor="light"
-                />
-                <InputText
-                  inputType="text"
-                  labelText="bank.code"
-                  labelType="floating"
-                  color="light"
-                  labelColor="light"
-                />
-                <InputText
-                  inputType="text"
-                  labelText="bank.holderName"
-                  labelType="floating"
-                  color="light"
-                  labelColor="light"
-                />
-                <InputText
-                  inputType="text"
-                  labelText="bank.number"
-                  labelType="floating"
-                  color="light"
-                  labelColor="light"
-                />
-                <InputText
-                  inputType="text"
-                  labelText="bank.mobile"
-                  labelType="floating"
-                  color="light"
-                  labelColor="light"
-                />
-                <InputText
-                  inputType="text"
-                  labelText="bank.amount"
-                  labelType="floating"
-                  color="light"
-                  labelColor="light"
-                />
-                <InputText
-                  inputType="text"
-                  labelText="bank.remark"
-                  labelType="floating"
-                  color="light"
-                  labelColor="light"
-                />
-                <div className="clear-button">
-                  <div style={{ marginTop: '13px' }}>
-                    <ButtonConmponent
-                      buttonLabel="agent.conform"
-                      size="block"
-                    />
+              <div className='agentDetails-wrapper'>
+                <IonText className="agentDetails-header-text"><span className="agent_text"><Translate message='agent.country'/></span></IonText>
+                <div className="agentDetails-message">
+                  <IonText className="nameMessage">
+                    {countrySelected}
+                  </IonText>
+                </div>
+                <IonText className="agentDetails-header-text"><span className="agent_text"><Translate message='agent.code'/></span></IonText>
+                <div className="agentDetails-message">
+                  <IonText className="nameMessage">
+                    {agentCode}
+                  </IonText>
+                </div>
+                <IonText className="agentDetails-header-text"><span className="agent_text"><Translate message='agent.holder'/></span></IonText>
+                <div className="agentDetails-message">
+                  <IonText className="nameMessage">
+                    {accountHolderName}
+                  </IonText>
+                </div>
+                <IonText className="agentDetails-header-text"><span className="agent_text"><Translate message='agent.number'/></span></IonText>
+                <div className="agentDetails-message">
+                  <IonText className="nameMessage">
+                    {accountNumber}
+                  </IonText>
+                </div>
+                <IonText className="agentDetails-header-text"><span className="agent_text"><Translate message='agent.mob'/></span></IonText>
+                <div className="agentDetails-message">
+                  <IonText className="nameMessage">
+                    {mobileNumber}
+                  </IonText>
+                </div>
+                <IonText className="agentDetails-header-text"><span className="agent_text"><Translate message='agent.amount'/></span></IonText>
+                <div className="agentDetails-message">
+                  <IonText className="nameMessage">
+                    {amount}
+                  </IonText>
+                </div>
+                <IonText className="agentDetails-header-text"><span className="agent_text"><Translate message='agent.remark'/></span></IonText>
+                <div className="agentDetails-message">
+                  <IonText className="nameMessage">
+                    {remarks}
+                  </IonText>
+                </div>
+                <div className="agentDetails-button">
+                  <div style={{marginTop: '13px'}}>
+                    <ButtonConmponent buttonLabel='agent.conform' size='block'  clickHandler={handleSucess}/>
                   </div>
                 </div>
               </div>
