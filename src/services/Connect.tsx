e33b537a701bf026d3d1b519f792e9465abeec37
nehatedroox <neha.tedroox@123>
2021-03-03 15:58:34 +0530

    Bank transfer api integration

--- src/services/Connect.tsx
+++ src/services/Connect.tsx
@@ -21,25 +21,60 @@ export const authenticate = async (
   return result;
 };
 
-
 export const authenticationForRegister = async (
-  fullName:string,
-  gender:string,
-  mobileNo:string,
-  password:string,
-   
+  fullName: string,
+  gender: string,
+  mobileNo: string,
+  password: string
 ): Promise<any> => {
   const result = await axios({
-    url:'http://ec2-65-1-95-227.ap-south-1.compute.amazonaws.com:8000/api/v1/register',
+    url:
+      'http://ec2-65-1-95-227.ap-south-1.compute.amazonaws.com:8000/api/v1/register',
     method: 'post',
     headers: {
       'Content-Type': 'application/json',
     },
     data: JSON.stringify({
-      name:fullName,
-      gender:gender,
-      mobile_number:mobileNo,
-      password:password
+      name: fullName,
+      gender: gender,
+      mobile_number: mobileNo,
+      password: password,
+    }),
+  });
+  console.log('result: ', result);
+
+  return result;
+};
+
+//Authentication For Bank Transfer
+export const authenticationForBankTransfer = async (
+  user_id: string,
+  destination: string,
+  holderName: string,
+  accountNumber: string,
+  mobileNo: string,
+  amount: string,
+  remarks: string
+): Promise<any> => {
+  const result = await axios({
+    url:
+      'http://ec2-65-1-95-227.ap-south-1.compute.amazonaws.com:8000/api/v1/bank_transfer',
+    method: 'post',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: JSON.stringify({
+      bank_transfer: [
+        {
+          user_id: user_id,
+          destination_bank: destination,
+          account_holder_name: holderName,
+          account_number: accountNumber,
+          mobile_number: mobileNo,
+          amount: amount,
+          remarks: remarks,
+        },
+      ],
     }),
   });
   console.log('result: ', result);
