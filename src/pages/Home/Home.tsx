531e7b32fee718bd7a3d0b21d490eb53cc0f4762
Ashish Kumar <ashish23.kumar@aricent.com>
2021-03-04 02:12:02 +0530

    added otp and push notification feature

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,12 @@
 import React, { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom';
+import {
+  Plugins,
+  PushNotification,
+  PushNotificationToken,
+  PushNotificationActionPerformed,
+} from '@capacitor/core';
 import {
   IonPage,
   IonContent,
@@ -18,20 +25,79 @@ import {
   ShoppingSection,
   LoanSection,
   UtilitiesSection,
+  LoaderComponent,
 } from '../../components';
 
 import './Home.scss';
 import { Translate } from '../../i18n/formatMessages';
 import { CloseBarIcon } from '../../assets/Icons';
+import { updateToast } from '../../redux/actions';
 
 const HomePage: React.FC = () => {
   const history = useHistory();
+  const dispatch = useDispatch();
   const [balance, setBalance] = useState(0);
   const [expandOptions, setExpandOptions] = useState(false);
-
+  const [initializing, setInitializing] = useState(false);
+  const { PushNotifications } = Plugins;
   useEffect(() => {
     setBalance(22090);
+    askPushPermission();
   }, []);
+
+  const askPushPermission = () => {
+    console.log('Asking for permission for push notification');
+    // Request permission to use push notifications
+    // iOS will prompt user and return if they granted permission or not
+    // Android will just grant without prompting
+    PushNotifications.requestPermission().then((result) => {
+      if (result.granted) {
+        console.log('User accepted the permission');
+        setInitializing(true);
+        PushNotifications.register();
+        registerListener();
+      } else {
+        const data = {
+          showToast: true,
+          toastMessage: 'Push notification registration failed',
+          position: 'top',
+          duration: '10000',
+        };
+        dispatch(updateToast(data));
+        console.log('Declined the permission');
+      }
+    });
+  };
+
+  const registerListener = () => {
+    console.log('Registering listner for status of registration');
+    // On success, we should be able to receive notifications
+    PushNotifications.addListener(
+      'registration',
+      (token: PushNotificationToken) => {
+        console.log('Push registration success, token: ', token.value);
+      }
+    );
+    // Some issue with our setup and push will not work
+    PushNotifications.addListener('registrationError', (error: any) => {
+      console.log('Error on registration: ', JSON.stringify(error));
+    });
+    PushNotifications.addListener(
+      'pushNotificationReceived',
+      (notification: PushNotification) => {
+        console.log('Push received: ', JSON.stringify(notification));
+      }
+    );
+    // Method called when tapping on a notification
+    PushNotifications.addListener(
+      'pushNotificationActionPerformed',
+      (notification: PushNotificationActionPerformed) => {
+        console.log('Push action performed: ', JSON.stringify(notification));
+      }
+    );
+    setInitializing(false);
+  };
+
   const toggleExpandOptions = () => {
     console.log('Hello');
     setExpandOptions(!expandOptions);
@@ -42,97 +108,108 @@ const HomePage: React.FC = () => {
   };
 
   return (
-    <IonApp className="home-wrapper">
-      <IonPage>
-        <HeaderComponent
-          headerLable={'common.header'}
-          showMenu={true}
-          showNotification={true}
-          notificationHandler={showNotifications}
-        />
-        <IonContent className="home-wrapper">
-          <div className="balance-check-section">
-            <div
-              className="common-ion-text"
-              style={{
-                backgroundColor: '#ffffff',
-                borderRadius: '7px 0px 0px 7px',
-              }}
-            >
-              <IonText
-                className="balance-wrapper-text"
-                style={{ color: '#000000' }}
-              >
-                <Translate message="home.balanceLabel" />
-              </IonText>
-            </div>
-            <div className="arrow_box"></div>
-            <div
-              className="common-ion-text"
-              style={{
-                backgroundColor: '#004777',
-                borderRadius: '0px 7px 7px 0px',
-              }}
-            >
-              <IonText
-                className="balance-wrapper-text"
-                style={{ color: '#ffffff' }}
-              >
-                {balance}
-              </IonText>
-            </div>
-          </div>
-          <SlidesComponent />
+    <>
+      <LoaderComponent
+        showLoading={initializing}
+        loaderMessage={'Preparing...'}
+      />
+      <IonApp className="home-wrapper">
+        <IonPage>
+          <HeaderComponent
+            headerLable={'common.header'}
+            showMenu={true}
+            showNotification={true}
+            notificationHandler={showNotifications}
+          />
 
-          <div
-            className={
-              expandOptions
-                ? 'services-options-wrapper fullHeight'
-                : 'services-options-wrapper'
-            }
-          >
-            <IonCard
-              className="service-card-wrapper"
-              style={
-                expandOptions ? { overflow: 'scroll' } : { overflow: 'hidden' }
+          <IonContent className="home-wrapper">
+            <div className="balance-check-section">
+              <div
+                className="common-ion-text"
+                style={{
+                  backgroundColor: '#ffffff',
+                  borderRadius: '7px 0px 0px 7px',
+                }}
+              >
+                <IonText
+                  className="balance-wrapper-text"
+                  style={{ color: '#000000' }}
+                >
+                  <Translate message="home.balanceLabel" />
+                </IonText>
+              </div>
+              <div className="arrow_box"></div>
+              <div
+                className="common-ion-text"
+                style={{
+                  backgroundColor: '#004777',
+                  borderRadius: '0px 7px 7px 0px',
+                }}
+              >
+                <IonText
+                  className="balance-wrapper-text"
+                  style={{ color: '#ffffff' }}
+                >
+                  {balance}
+                </IonText>
+              </div>
+            </div>
+            <SlidesComponent />
+
+            <div
+              className={
+                expandOptions
+                  ? 'services-options-wrapper fullHeight'
+                  : 'services-options-wrapper'
               }
             >
-              <IonCardContent
-                style={{ paddingBottom: '0px', paddingTop: '0px' }}
+              <IonCard
+                className="service-card-wrapper"
+                style={
+                  expandOptions
+                    ? { overflow: 'scroll' }
+                    : { overflow: 'hidden' }
+                }
               >
-                {expandOptions && (
-                  <div className="close-bar-icon">
-                    <button onClick={toggleExpandOptions}>
-                      <CloseBarIcon />
-                    </button>
-                  </div>
-                )}
+                <IonCardContent
+                  style={{ paddingBottom: '0px', paddingTop: '0px' }}
+                >
+                  {expandOptions && (
+                    <div className="close-bar-icon">
+                      <button onClick={toggleExpandOptions}>
+                        <CloseBarIcon />
+                      </button>
+                    </div>
+                  )}
 
-                <IonGrid>
-                  <ShoppingSection expanded={expandOptions} />
+                  <IonGrid>
+                    <ShoppingSection expanded={expandOptions} />
 
-                  <LoanSection expanded={expandOptions} />
-                  <UtilitiesSection expanded={expandOptions} />
-                  <div className="see-more-section">
-                    <IonButton onClick={toggleExpandOptions}>
-                      <IonIcon
-                        slot="end"
-                        icon={expandOptions ? caretUpOutline : caretDownOutline}
-                      />
-                      {!expandOptions ? (
-                        <Translate message="home.seeAll" />
-                      ) : (
-                        <Translate message="home.collapse" />
-                      )}
-                    </IonButton>
-                  </div>
-                </IonGrid>
-              </IonCardContent>
-            </IonCard>
-          </div>
-        </IonContent>
-      </IonPage>
-    </IonApp>
+                    <LoanSection expanded={expandOptions} />
+                    <UtilitiesSection expanded={expandOptions} />
+                    <div className="see-more-section">
+                      <IonButton onClick={toggleExpandOptions}>
+                        <IonIcon
+                          slot="end"
+                          icon={
+                            expandOptions ? caretUpOutline : caretDownOutline
+                          }
+                        />
+                        {!expandOptions ? (
+                          <Translate message="home.seeAll" />
+                        ) : (
+                          <Translate message="home.collapse" />
+                        )}
+                      </IonButton>
+                    </div>
+                  </IonGrid>
+                </IonCardContent>
+              </IonCard>
+            </div>
+          </IonContent>
+        </IonPage>
+      </IonApp>
+    </>
   );
 };
 
--- src/pages/Otp/OtpPage.tsx
+++ src/pages/Otp/OtpPage.tsx
@@ -1,23 +1,70 @@
 import React, { useState } from 'react';
-import { useHistory } from 'react-router-dom';
+// import { useHistory } from "react-router-dom";
 import { IonPage, IonContent, IonText, IonApp } from '@ionic/react';
+import { FirebaseAuthentication } from '@ionic-native/firebase-authentication';
+import 'firebase/auth';
 import OtpInput from 'react-otp-input';
 import { Translate } from '../../i18n/formatMessages';
-import { ButtonConmponent } from '../../components';
+import { ButtonConmponent, LoaderComponent } from '../../components';
 
 import './OtpPage.scss';
 
 const OtpPage: React.FC = () => {
-  const [otpText1, setOtpText1] = useState('');
-  const history = useHistory();
-  function handleVerifyOtp() {
-    history.push('/mpin');
-  }
+  const [otpText, setOtpText] = useState('');
+  // const history = useHistory();
+  const [verificationId, setVerificationId] = useState('');
+  const [otpValue, setOtpValue] = useState('');
+  const [otpReceived, setOtpReceived] = useState(false);
+  const [showLoading, setShowLoading] = useState(false);
+  // history.push("/mpin");
+
   const onOtpEnter = (otp: any) => {
-    setOtpText1(otp);
+    setOtpText(otp);
+    const finalOtp = otpValue.concat(otp);
+    setOtpValue(finalOtp);
+  };
+
+  function sendOTP() {
+    console.log('Sending OTP now');
+    setShowLoading(true);
+    FirebaseAuthentication.verifyPhoneNumber('+918892050268', 30000)
+      .then((verificationId: any) => {
+        setShowLoading(false);
+        console.log('Received verificationId: ', verificationId);
+        setOtpReceived(true);
+        setVerificationId(verificationId);
+      })
+      .catch(function (err: any) {
+        setShowLoading(false);
+        console.log('phoner number verification failed', err);
+      });
+  }
+
+  const handleVerifyOtp = () => {
+    console.log('otp: ', otpText);
+    console.log('verificationId: ', verificationId);
+    setShowLoading(true);
+    // const signInCredential = firebase.auth.PhoneAuthProvider.credential(
+    //   verificationId,
+    //   otpValue
+    // );
+    FirebaseAuthentication.signInWithVerificationId(
+      verificationId,
+      otpText
+    ).then(
+      (response) => {
+        console.log('opt verification status: ', response);
+        setShowLoading(false);
+      },
+      (error) => {
+        setShowLoading(false);
+        console.log('Error: ', error);
+      }
+    );
   };
   return (
     <>
+      <LoaderComponent showLoading={showLoading} loaderMessage="Wait..." />
       <IonApp>
         <IonPage>
           <IonContent>
@@ -27,41 +74,72 @@ const OtpPage: React.FC = () => {
                   <Translate message="otp.pageHeader" />
                 </IonText>
               </div>
-              <div className="page-sub-header">
-                <div className="innercontainer">
-                  <IonText>
-                    <Translate
-                      message="otp.pageSubHeader"
-                      value={{ contact: '8892050268' }}
+              {!otpReceived && (
+                <>
+                  <div className="innercontainer">
+                    <IonText>
+                      <Translate
+                        message="otp.sendOtpInfo"
+                        value={{ contact: '8892050268' }}
+                      />
+                    </IonText>
+                  </div>
+                  <div className="confirm-btn-wrapper">
+                    <ButtonConmponent
+                      buttonLabel="otp.send"
+                      size="block"
+                      clickHandler={sendOTP}
                     />
-                  </IonText>
-                </div>
-              </div>
-              <div className="field-container">
-                <OtpInput
-                  value={otpText1}
-                  onChange={onOtpEnter}
-                  numInputs={4}
-                  containerStyle="otp-field-wrapper"
-                  isInputNum={true}
-                  inputStyle="otp-field-input"
-                />
-              </div>
-              <div className="resend-link-container">
-                <IonText>
-                  <Translate message="otp.resendText" />
-                  <a>
-                    <Translate message="otp.resendLinkText" />
-                  </a>
-                </IonText>
-              </div>
-              <div className="confirm-btn-wrapper">
-                <ButtonConmponent
-                  buttonLabel="common.continue"
-                  size="block"
-                  clickHandler={handleVerifyOtp}
-                />
-              </div>
+                  </div>
+                </>
+              )}
+              {otpReceived && (
+                <>
+                  <div className="page-sub-header">
+                    <div className="innercontainer">
+                      <IonText>
+                        <Translate
+                          message="otp.pageSubHeader"
+                          value={{ contact: '8892050268' }}
+                        />
+                      </IonText>
+                    </div>
+                  </div>
+
+                  <div className="field-container">
+                    <OtpInput
+                      value={otpText}
+                      onChange={onOtpEnter}
+                      numInputs={6}
+                      containerStyle="otp-field-wrapper"
+                      isInputNum={true}
+                      inputStyle="otp-field-input"
+                    />
+                  </div>
+                  <div className="resend-link-container">
+                    <IonText>
+                      <Translate message="otp.resendText" />
+                      <a onClick={sendOTP}>
+                        <Translate message="otp.resendLinkText" />
+                      </a>
+                    </IonText>
+                  </div>
+                  <div className="confirm-btn-wrapper">
+                    <ButtonConmponent
+                      buttonLabel="common.continue"
+                      size="block"
+                      clickHandler={handleVerifyOtp}
+                    />
+                    {/* <button
+                  disabled
+                  id="sign-in-button"
+                  style={{ display: "none" }}
+                >
+                  Sign-in
+                </button> */}
+                  </div>
+                </>
+              )}
             </div>
           </IonContent>
         </IonPage>
