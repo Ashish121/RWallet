4709a2c4a4df6417b56e1ddd654ee6c12b778c7b
nehatedroox <neha.tedroox@123>
2021-03-19 22:18:07 +0530

    Ineternet payment page setup

--- src/pages/Utilitys/InternetPayment/InternetPayment.tsx
+++ src/pages/Utilitys/InternetPayment/InternetPayment.tsx
@@ -1,15 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import './InternetPayment.scss';
-import {
-  IonPage,
-  IonContent,
-  IonText,
-  IonApp,
-} from '@ionic/react';
+import { useHistory } from 'react-router-dom';
+import { IonPage, IonContent, IonText, IonApp } from '@ionic/react';
 import { Translate } from '../../../i18n/formatMessages';
-import { CustomAccordion, HeaderComponent  } from '../../../components';
+import {
+  CustomAccordionForRecharge,
+  HeaderComponent,
+} from '../../../components';
 
 const InternetPayment: React.FC = () => {
+  const history = useHistory();
   const [accordionDetails, setAccordionDetails] = useState([{}]);
   useEffect(() => {
     const timeout = setTimeout(() => {
@@ -26,53 +26,70 @@ const InternetPayment: React.FC = () => {
         id: 1,
         title: 'World link',
         showDetails: false,
+        inputfield1: 'Customer ID/Username',
+        inputfield2: 'Amount',
       },
       {
         id: 2,
         title: 'Via net',
         showDetails: false,
+        inputfield1: 'Customer ID/Username',
+        inputfield2: 'Amount',
       },
 
       {
         id: 3,
         title: 'Subisu Internet',
         showDetails: false,
+        inputfield1: 'Customer ID/Username',
+        inputfield2: 'Amount',
       },
-      
+
       {
         id: 4,
         title: 'Classic Tech',
         showDetails: false,
+        inputfield1: 'Customer ID/Username',
+        inputfield2: 'Amount',
       },
-      
+
       {
         id: 5,
         title: 'Nepal Telecom FTTH',
         showDetails: false,
+        inputfield1: 'Customer ID/Username',
+        inputfield2: 'Amount',
       },
       {
         id: 5,
         title: 'Nepal Telecom WIMAX',
         showDetails: false,
+        inputfield1: 'Customer ID/Username',
+        inputfield2: 'Amount',
       },
-    
-    
     ];
     setAccordionDetails(data);
   }, []);
 
+  function goBack() {
+    history.replace('/tabs/home');
+  }
   return (
     <>
       <IonApp>
         <IonPage>
-          <HeaderComponent headerLable="common.header" />
+          <HeaderComponent
+            headerLable="common.header"
+            showBackButton={true}
+            handler={goBack}
+          />
           <IonContent>
             <div className="container">
               <IonText className="InternetPayment-text-area">
-                <Translate message="UtlityInternetPayment" />
+                <Translate message="Internet Payment" />
               </IonText>
-              <div className="Internetpayment-wrapper">
-                <CustomAccordion accordionData={accordionDetails} />
+              <div className="InternetPaymentWrapper ">
+                <CustomAccordionForRecharge accordionData={accordionDetails} />
               </div>
             </div>
           </IonContent>
@@ -82,4 +99,4 @@ const InternetPayment: React.FC = () => {
   );
 };
 
-export {InternetPayment };
+export { InternetPayment };
