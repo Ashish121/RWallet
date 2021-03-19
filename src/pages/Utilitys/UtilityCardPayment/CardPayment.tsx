0fb6f3e8a4ee0f72f5533b4ba1da24136aa47da9
nehatedroox <neha.tedroox@123>
2021-03-19 22:58:36 +0530

    card payment page setup

--- src/pages/Utilitys/UtilityCardPayment/CardPayment.tsx
+++ src/pages/Utilitys/UtilityCardPayment/CardPayment.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import { IonPage, IonContent, IonText, IonApp } from '@ionic/react';
 import { Translate } from '../../../i18n/formatMessages';
@@ -6,35 +6,64 @@ import {
   ButtonConmponent,
   InputText,
   HeaderComponent,
+  SelectMenu,
 } from '../../../components';
 import './CardPayment.scss';
 
 const CardPayment: React.FC = () => {
   const history = useHistory();
+  // const [gender, setGender] = useState("");
+  const [genderDetails, setGenderDetails] = useState([{}]);
 
+  useEffect(() => {
+    const array = [
+      {
+        value: 'bank-A',
+        label: 'Bank-A',
+      },
+      {
+        value: 'bank-B',
+        label: 'Bank-B',
+      },
+      {
+        value: 'bank-C',
+        label: 'Bank-C',
+      },
+    ];
+    setGenderDetails(array);
+  }, []);
   function handleproceed() {
     console.log('Handling registration');
-    history.replace('/');
+    history.replace('/tabs/home');
   }
-
+  function goBack() {
+    history.replace('/tabs/home');
+  }
+  function onGenderSelect(gender: any) {
+    console.log('Selected value: ', gender);
+    // setGender(gender);
+  }
   return (
     <>
       <IonApp>
         <IonPage>
-          <HeaderComponent headerLable="common.header" />
+          <HeaderComponent
+            headerLable="common.header"
+            showBackButton={true}
+            handler={goBack}
+          />
           <IonContent>
             <div className="container">
               <IonText className="credit-card-text-area">
                 <Translate message="UtilityInternetPayment" />
               </IonText>
               <div className="credit-card-wrapper">
-                <InputText
-                  inputType="text"
-                  labelText="UtilityBankName"
-                  labelType="floating"
-                  color="light"
-                  labelColor="light"
+                <SelectMenu
+                  label="UtilityBankName"
+                  onSelect={onGenderSelect}
+                  array={genderDetails}
                 />
+
                 <InputText
                   inputType="text"
                   labelText="UtilityCardNumber"
@@ -50,18 +79,20 @@ const CardPayment: React.FC = () => {
                   labelColor="light"
                 />
 
-                <div className="card-button-last">
-                  <ButtonConmponent
-                    buttonLabel="UtilityCardClear"
-                    size="block"
-                  />
-                </div>
-                <div className="card-submit">
-                  <ButtonConmponent
-                    buttonLabel="UtilityConfirm"
-                    size="block"
-                    clickHandler={handleproceed}
-                  />
+                <div className="buttonElement">
+                  <div className="cardClearButton">
+                    <ButtonConmponent
+                      buttonLabel="UtilityCardClear"
+                      size="block"
+                    />
+                  </div>
+                  <div className="cardSubmitButton">
+                    <ButtonConmponent
+                      buttonLabel="UtilityConfirm"
+                      size="block"
+                      clickHandler={handleproceed}
+                    />
+                  </div>{' '}
                 </div>
               </div>
             </div>
