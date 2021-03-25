08732b85f65757bf173c8767817dfd8511c22ce7
Ashish Kumar <ashish23.kumar@aricent.com>
2021-03-25 11:44:21 +0530

    bug fixes

--- src/components/Slides/Slides.tsx
+++ src/components/Slides/Slides.tsx
@@ -1,50 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { IonSlides, IonSlide, IonCard, IonCardContent } from '@ionic/react';
 
 // Optional parameters to pass to the swiper instance.
 // See http://idangero.us/swiper/api/ for valid options.
 const slideOpts = {
   initialSlide: 1,
+  autoplay: true,
   speed: 400,
 };
 
 const SlidesComponent: React.FC<any> = () => {
+  useEffect(() => {}, []);
   return (
     <IonSlides pager={true} options={slideOpts}>
       <IonSlide>
         <IonCard>
-          <IonCardContent>
-            Keep close to Nature's heart... and break clear away, once in
-            awhile, and climb a mountain or spend a week in the woods. Wash your
-            spirit clean.
-          </IonCardContent>
+          <IonCardContent>Image 1</IonCardContent>
         </IonCard>
       </IonSlide>
       <IonSlide>
         <IonCard>
-          <IonCardContent>
-            Keep close to Nature's heart... and break clear away, once in
-            awhile, and climb a mountain or spend a week in the woods. Wash your
-            spirit clean.
-          </IonCardContent>
+          <IonCardContent>Image 2</IonCardContent>
         </IonCard>
       </IonSlide>
       <IonSlide>
         <IonCard>
-          <IonCardContent>
-            Keep close to Nature's heart... and break clear away, once in
-            awhile, and climb a mountain or spend a week in the woods. Wash your
-            spirit clean.
-          </IonCardContent>
+          <IonCardContent>Image 3</IonCardContent>
         </IonCard>
       </IonSlide>
       <IonSlide>
         <IonCard>
-          <IonCardContent>
-            Keep close to Nature's heart... and break clear away, once in
-            awhile, and climb a mountain or spend a week in the woods. Wash your
-            spirit clean.
-          </IonCardContent>
+          <IonCardContent>Image 4</IonCardContent>
         </IonCard>
       </IonSlide>
     </IonSlides>
