76487110820a877d6e59e16108bb96815876b29a
nehatedroox <neha.tedroox@123>
2021-05-19 21:50:30 +0530

    shopping page section changes and Image slider

--- src/components/Slides/Slides.tsx
+++ src/components/Slides/Slides.tsx
@@ -1,36 +1,63 @@
 import React, { useEffect } from 'react';
-import { IonSlides, IonSlide, IonCard, IonCardContent } from '@ionic/react';
+import {
+  IonSlides,
+  IonSlide,
+  IonCard,
+  IonCardContent,
+  IonImg,
+} from '@ionic/react';
 
 // Optional parameters to pass to the swiper instance.
 // See http://idangero.us/swiper/api/ for valid options.
+
+interface sliderProps {
+  value?: any;
+}
+
 const slideOpts = {
   initialSlide: 1,
   autoplay: true,
   speed: 400,
 };
 
-const SlidesComponent: React.FC<any> = () => {
+const SlidesComponent: React.FC<sliderProps> = ({ value }) => {
   useEffect(() => {}, []);
   return (
     <IonSlides pager={true} options={slideOpts}>
       <IonSlide>
         <IonCard>
-          <IonCardContent>Image 1</IonCardContent>
+          <IonCardContent>
+            <div style={{ backgroundColor: 'white', borderRadius: '7px' }}>
+              <IonImg src={value} style={{ padding: '30px', margin: '10px' }} />
+            </div>
+          </IonCardContent>
         </IonCard>
       </IonSlide>
       <IonSlide>
         <IonCard>
-          <IonCardContent>Image 2</IonCardContent>
+          <IonCardContent>
+            <div style={{ backgroundColor: 'white', borderRadius: '7px' }}>
+              <IonImg src={value} style={{ padding: '30px', margin: '10px' }} />
+            </div>
+          </IonCardContent>
         </IonCard>
       </IonSlide>
       <IonSlide>
         <IonCard>
-          <IonCardContent>Image 3</IonCardContent>
+          <IonCardContent>
+            <div style={{ backgroundColor: 'white', borderRadius: '7px' }}>
+              <IonImg src={value} style={{ padding: '30px', margin: '10px' }} />
+            </div>
+          </IonCardContent>
         </IonCard>
       </IonSlide>
       <IonSlide>
         <IonCard>
-          <IonCardContent>Image 4</IonCardContent>
+          <IonCardContent>
+            <div style={{ backgroundColor: 'white', borderRadius: '7px' }}>
+              <IonImg src={value} style={{ padding: '30px', margin: '10px' }} />
+            </div>
+          </IonCardContent>
         </IonCard>
       </IonSlide>
     </IonSlides>
